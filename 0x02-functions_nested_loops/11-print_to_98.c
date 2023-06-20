e8ffc6df852c0d449376eacf08163c92e46e3a48
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2023-06-20 08:29:57 -0700

    some corrections

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - prints from n to 98
+ * @n: starting number
+ * Return: void (nothing)
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	printf("98");
+	putchar('\n');
+}
