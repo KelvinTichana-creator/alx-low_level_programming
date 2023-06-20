4ad56db6cdf7f5a31b5d387d1171d9f00b1995b2
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2023-06-20 08:43:12 -0700

    some changes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,17 @@
  */
 int print_last_digit(int n)
 {
-	if (n >= 0 && n <= 9)
+	int i = 0;
+
+	i = n%10;
+	if (i < 0)
 	{
-		return (n);
+		_putchar(-i + 48);
+		return (-i);
 	}
 	else
 	{
-		n = n % 10;
-		return (n);
+		_putchar(i + 48);
+		return (i);
 	}
 }
