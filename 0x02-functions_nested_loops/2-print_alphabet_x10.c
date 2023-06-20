ac0fff4f794088594a070afc9e0f16bfabc222f1
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2023-06-20 06:19:16 -0700

    10 times soup of alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	char c = 'a';
+	char j;
 
-	while (i < 10)
+  for (i = 0; i < 10; i++)
+  {
+	for (j = 'a'; j <= 'z'; j++)
 	{
-		while ( c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		i++;
-		_putchar('\n');
+		_putchar(j);
 	}
+	_putchar('\n');
+  }
 }
