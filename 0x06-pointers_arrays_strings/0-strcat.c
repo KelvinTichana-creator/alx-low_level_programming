236aeec2dcb3c83865b98a1c508e3b1ce21674cf
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2023-06-29 07:56:39 -0700

    mandatory tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concatenates 2 strings.
+ * @dest: string2
+ * @src: string1
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lenD, lenS;
+
+	lenD = 0;
+	lenS = 0;
+
+	while (*(dest + lenD) != '\0')
+		lenD++;
+
+	while (*(src + lenS) != '\0' && lenD < 97)
+	{
+		*(dest + lenD) = *(src + lenS);
+		lenD++;
+		lenS++;
+	}
+	*(dest + lenD) = '\0';
+	return (dest);
+}
