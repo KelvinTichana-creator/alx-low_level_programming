2332d669b2216fab75d383cf33ca9e19f4108424
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2023-07-07 09:53:57 -0700

    corrections

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - function to print
- *
- * @str: character
- *
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
-
 void _puts(char *str)
 {
-	int a;
+	int i;
 
-	for (a = 0; str[a] != '\0'; a++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[a]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
