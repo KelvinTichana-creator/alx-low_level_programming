23e59a60962dd97739cefcc8de839969c9ca36d3
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2023-08-03 11:59:16 -0700

    fixing little endian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness of the machine.
- *
- * Return: 0 if big endian, 1 if little endian.
+ * get_endianness - check endianness of the mchine
+ * Return: 0 if big endian, or 1 if little endian
  */
 int get_endianness(void)
 {
-	unsigned int num = 1;
-	char *endian = (char *)&num;
-	return (*endian);
-}
+	int num;
 
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
