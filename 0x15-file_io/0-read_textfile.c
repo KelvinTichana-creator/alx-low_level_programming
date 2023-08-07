f63fcbdddb93f74fd78c1cc3be5b7c6e92a1e3c2
Kelvin Carrington Tichana <kelvin.tichana@ashesi.edu.gh>
2023-08-07 13:39:22 -0700

    file handling C

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints its contents to stdout
+ * @filename: Pointer to the name of the text file.
+ * @letters: Number of letters to read and print.
+ * Return: Number of letters read and printed, or 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, let, w;
+	char *text;
+
+	if (filename == NULL)
+		return (0);
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	let = read(file, text, letters);
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+	free(text);
+
+	return (w);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error_exit - Print an error message and exit with the given code.
+ * @exit_code: The exit code.
+ * @format: The format string for the error message.
+ * @...: Additional arguments for the format string.
+ */
+void error_exit(int exit_code, const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	dprintf(2, format, args);
+	va_end(args);
+	exit(exit_code);
+}
+
+/**
+ * main - Copy the content of a file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success, other values on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit(97, "Usage: %s file_from file_to\n", argv[0]);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+	if (bytes_read == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+	return (0);
+}
+
